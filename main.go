5dcdac85133c78c6189f0ea6f13a346484dd6faa
nabin-bh <nabinbhandari104@gmail.com>
2022-06-16 17:05:02 +0545

    static data save in db

--- main.go
+++ main.go
@@ -1,129 +1,45 @@
 package main
 
 import (
-	"net/http"
-	"errors"
-	"github.com/gin-gonic/gin"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
-type book struct {
-	ID       string `json:"id"`
-	Title    string `json:"title"`
-	Author   string `json:"author"`
-	Quantity int    `json:"quantity"`
-}
-
-var books = []book{
-	{ID: "1", Title: "Book 1", Author: "Author of Book 1", Quantity: 2},
-	{ID: "2", Title: "Book 2", Author: "Author of Book 2", Quantity: 5},
-	{ID: "3", Title: "Book 3", Author: "Author of Book 3", Quantity: 6},
-}
-
-func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, books)
-}
-
-func bookById(c *gin.Context) {
-	id := c.Param("id")
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-		return
-	}
-
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func checkoutBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
-		return
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-		return
-	}
-
-	if book.Quantity <= 0 {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
-		return
-	}
-
-	book.Quantity -= 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func returnBook(c *gin.Context) {
-	id, ok := c.GetQuery("id")
-
-	if !ok {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
-		return
-	}
-
-	book, err := getBookById(id)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-		return
-	}
-
-	book.Quantity += 1
-	c.IndentedJSON(http.StatusOK, book)
-}
-
-func getBookById(id string) (*book, error) {
-	for i, b := range books {
-		if b.ID == id {
-			return &books[i], nil
-		}
-	}
-
-	return nil, errors.New("book not found")
-}
-
-func createBook(c *gin.Context) {
-	var newBook book
-
-	if err := c.BindJSON(&newBook); err != nil {
-		return
-	}
-
-	books = append(books, newBook)
-	c.IndentedJSON(http.StatusCreated, newBook)
-}
-
 func main() {
-	router := gin.Default()
-	router.GET("/books", getBooks)
-	router.GET("/books/:id", bookById)
-	router.POST("/books", createBook)
-	router.PATCH("/checkout", checkoutBook)
-	router.PATCH("/return", returnBook)
-	router.Run("localhost:8005")
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/demo_go")
+	defer db.Close()
+
+	if err != nil {
+		log.Println("hello")
+		log.Fatal(err)
+	}
+
+	sql := "INSERT INTO students(email, first_name, last_name) VALUES ('admin@gmail.com', 'admin','admin')"
+
+	res, err := db.Exec(sql)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	lastId, err := res.LastInsertId()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The last inserted row id: %d\n", lastId)
 }
 
-func loginpagedesignandmake()
-{
-    return "this is login page";
-}
-
-
-func logoutsystem()
-{
-    return "logout success";
-}
-
-// sample data for createBook
-// {
-//   "id": "4",
-//   "title": "Book 4",
-//   "author": "Author of Book 4",
-//   "quantity": 2
-// }
+// SQL QUERY
+// CREATE TABLE `students` (
+// 	`id` bigint NOT NULL AUTO_INCREMENT,
+// 	`email` varchar(255) DEFAULT NULL,
+// 	`first_name` varchar(255) DEFAULT NULL,
+// 	`last_name` varchar(255) DEFAULT NULL,
+// 	PRIMARY KEY (`id`)
+//   )
