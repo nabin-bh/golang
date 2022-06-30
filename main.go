c66a2850537f6664c1ad33bad73ecbaf9990bfb1
nabin-bh <nabinbhandari104@gmail.com>
2022-06-30 15:54:35 +0545

    fdff

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+type Todo struct {
+	Title string
+	Done  bool
+}
 
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+type Form struct {
+	Name    string
+	Address string
+}
+
+func insertIntoDB() {
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/demo_go")
 	defer db.Close()
 
@@ -32,14 +48,36 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("The last inserted row id: %d\n", lastId)
+	fmt.Printf("Data inserted success in row id: %d\n", lastId)
 }
 
-// SQL QUERY
-// CREATE TABLE `students` (
-// 	`id` bigint NOT NULL AUTO_INCREMENT,
-// 	`email` varchar(255) DEFAULT NULL,
-// 	`first_name` varchar(255) DEFAULT NULL,
-// 	`last_name` varchar(255) DEFAULT NULL,
-// 	PRIMARY KEY (`id`)
-//   )
+func main() {
+
+	tmpl := template.Must(template.ParseFiles("form.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+
+	showpagehtml := template.Must(template.ParseFiles("Show.html"))
+	http.HandleFunc("/store", func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatal(err)
+		}
+		myvar := map[string]interface{}{
+			"name":    r.Form.Get("name"),
+			"address": r.Form.Get("address"),
+		}
+		showpagehtml.Execute(w, myvar)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
