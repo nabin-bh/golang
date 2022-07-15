f33bf8df242fbb539e66ea92cd1c4dd317d089e9
nabin-bh <nabinbhandari104@gmail.com>
2022-07-15 16:41:37 +0545

    log file for go

--- controllers/checkout.go
+++ controllers/checkout.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+
+	"log"
+	"os"
 )
 
 type CreateCheckoutInput struct {
@@ -19,6 +22,21 @@ type CreateCheckoutInput struct {
 
 func Checkout(c *gin.Context) {
 	var input CreateCheckoutInput
+
+	//create your file with desired read/write permissions
+	f, err := os.OpenFile("go.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//defer to close when you're done with it, not because you think it's idiomatic!
+	defer f.Close()
+
+	//set output of logs to f
+	log.SetOutput(f)
+
+	//test case
+	log.Println(input)
 
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
--- main.go
+++ main.go
@@ -33,3 +33,15 @@ func main() {
 
 	r.Run(":8080")
 }
+
+//create your file with desired read/write permissions
+// f, err := os.OpenFile("go.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// //defer to close when you're done with it, not because you think it's idiomatic!
+// defer f.Close()
+// //set output of logs to f
+// log.SetOutput(f)
+// //test case
+// log.Println(input)
