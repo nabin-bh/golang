bb4251f0b4940b0c91c8ba284e0d842e8cc1d32f
nabin-bh <nabinbhandari104@gmail.com>
2022-08-17 17:33:00 +0200

    feat: db connection test

---
+++ test/bookAPI_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"golang/controllers"
+	"testing"
+)
+
+type CreateBookInput struct {
+	Name        string `json:"name"`
+	Category    string `json:"category"`
+	Author      string `json:"author"`
+	Description string `json:"description"`
+	Price       string `json:"price"`
+}
+
+func TestBookList(t *testing.T) {
+	result := controllers.BookList
+	println(result)
+}
+
+func TestCreateBook(t *testing.T) {
+	// controllers.CreateBook
+}
