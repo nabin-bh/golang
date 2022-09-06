d463cf9593eb1c7893e0cd48bd5dd128d586ab5e
nbn1 <nabinbhandari104@gmail.com>
2022-09-06 13:05:59 +0200

    fix: db connect for unit test env

--- test/dbconnection_test.go
+++ test/dbconnection_test.go
@@ -9,10 +9,4 @@ func TestDBConnection(t *testing.T) {
 	result := models.SetupDB()
 	println("result success")
 	println(result)
-
-	// if result != "0x1400023bad0" {
-	// 	t.Errorf("Database Connection FAILED")
-	// } else {
-	// 	t.Logf("Database Connection Pass")
-	// }
 }
