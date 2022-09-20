e77711929243079be4d658bd43f7859ea9827cbf
nabin-bh <nabinbhandari104@gmail.com>
2022-09-20 02:01:19 +0200

    fix: env for mac

--- test/dbconnection_test.go
+++ test/dbconnection_test.go
@@ -9,8 +9,9 @@ func TestDBConnection(t *testing.T) {
 	result := models.SetupDB()
 	println("result success")
 	println(result)
+	// test := "0x1400023bad0"
 
-	// if result != "0x1400023bad0" {
+	// if result != test {
 	// 	t.Errorf("Database Connection FAILED")
 	// } else {
 	// 	t.Logf("Database Connection Pass")
